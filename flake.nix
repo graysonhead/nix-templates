1573547a1bcd403ad50d81bd0d4e3abb9b78c5d4
Grayson Head <grayson@graysonhead.net>
2023-03-08 09:15:14 -0600

    Fixing devshell

--- flake.nix
+++ flake.nix
@@ -1,37 +1,39 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   description = "Grayson's flake templates";
-  outputs = { self, flake-utils, nixpkgs }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }:
     let
       pkgs = (import nixpkgs) {
-        inherit system;
+        system = "x86_64-linux";
       };
     in
     {
-      python-poetry = {
-        description = "Python project with poetry, vscode, and direnv";
-        path = ./python/poetry;
+      templates = {
+        python-poetry = {
+          description = "Python project with poetry, vscode, and direnv";
+          path = ./python/poetry;
+        };
+        rust-cargo2nix = {
+          description = "Rust project with cargo2nix";
+          path = ./rust/cargo2nix;
+        };
+        rust-bevy = {
+          description = "Rust and Bevy";
+          path = ./rust/bevy;
+        };
+        rust-naersk = {
+          description = "Rust Naersk";
+          path = ./rust/naersk;
+        };
       };
-      rust-cargo2nix = {
-        description = "Rust project with cargo2nix";
-        path = ./rust/cargo2nix;
+      devShell = {
+        x86_64-linux = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nixpkgs-fmt
+          ];
+        };
       };
-      rust-bevy = {
-        description = "Rust and Bevy";
-        path = ./rust/bevy;
-      };
-      rust-naersk = {
-        description = "Rust Naersk";
-        path = ./rust/naersk;
-      };
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          nixpkgs-fmt
-        ];
-      };
-    }
-  );
+    };
 }
