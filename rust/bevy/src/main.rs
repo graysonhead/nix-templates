ec8d475069ee8565a0f136caa0532e832d12f261
Grayson Head <grayson@graysonhead.net>
2023-06-20 19:53:05 -0500

    Rotate the Bevy Example

--- rust/bevy/src/main.rs
+++ rust/bevy/src/main.rs
@@ -5,8 +5,12 @@ fn main() {
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_scene)
+        .add_system(rotate_scene)
         .run();
 }
+
+#[derive(Reflect, Component)]
+pub struct Rotate {}
 
 fn spawn_scene(mut commands: Commands, assets: Res<AssetServer>) {
     let lighthouse = assets.load("lighthouse.glb#Scene0");
@@ -19,11 +23,13 @@ fn spawn_scene(mut commands: Commands, assets: Res<AssetServer>) {
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
     });
-    commands.spawn(SceneBundle {
-        scene: lighthouse,
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..Default::default()
-    });
+    commands
+        .spawn(SceneBundle {
+            scene: lighthouse,
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..Default::default()
+        })
+        .insert(Rotate{});
     let mut camera_transform = Transform::from_xyz(-5.0, 2.0, -5.0);
     let camera_direction =
         (Transform::from_xyz(0.0, 0.0, 0.0).translation - camera_transform.translation).normalize();
@@ -32,4 +38,10 @@ fn spawn_scene(mut commands: Commands, assets: Res<AssetServer>) {
         transform: camera_transform,
         ..Default::default()
     });
+}
+
+fn rotate_scene(mut scene_object: Query<(Entity, &Rotate, &mut Transform)>) {
+    for (_, _, mut transform) in &mut scene_object {
+        transform.rotate_y(0.1);
+    }
 }
